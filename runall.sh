02e3878376384d383c1a9188f93132c9959b2f9a
Juan Manuel Franco Patiño <45358012+juafrapat@users.noreply.github.com>
2019-01-02 17:00:07 +0100

    Add files via upload

--- runall.sh
+++ runall.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 FRESCO=frescox #Calling FRESCO
 destino=fort.4
-XSECS=xsec_completo.out # Energy--Xsec_elast---Xsec_absorp---Xsec_react---Xsec_total.
+XSECS=xsec_completo.out # Cross section output for each energy.
 XSECS2=xsec_estados.out # Data from FRESCO's output fort.13 [Xsec to all excited states one by one].
 fort=fort.13
 fort39=fort.39
+lista=lista.txt
 rm -f $destino
 rm -f $XSECS
 rm -f $XSECS2
+echo 'Energy Elastic    Absorption   Reaction    Total    (MeV/mb)' >> $XSECS
 while read input origen; do
  base=${input%%.*}
  output=$base.out
+# Comment next line if you will make a run without any excited band (only G.S band).
  ln -fs $origen $destino
-#  echo $input $output
   elab=`cat  $input | egrep elab | awk '{print $2 }'`
-  echo Running energy: $elab
+  echo Running energy: $elab MeV
   
   $FRESCO  < $input  > $output 
 
@@ -23,12 +25,16 @@ while read input origen; do
  xabs=`grep -v NaN $fort39 | awk '{print $2}'`
  xreac=`grep 0.0000 $fort | awk 'FNR ==2 {print $7}'`
  x_gs_0=`grep  0.5  $fort  | awk '{print $7}'`
-  echo 'Elastic:' $xel ' Reac:' $xreac  ' Abs:' $xabs #'0+gs: ' $x_gs_0
+  echo ""
+  echo "Elastic:" $xel mb 
+  echo 'Reaction:' $xreac mb
+  echo 'Absorption:' $xabs mb
+  echo ""
   echo $elab $xel $xabs $xreac $xtot  >> $XSECS
   echo $elab $x_gs_0 >> $XSECS2
-#  mv fort.56
+
   
-done < lista.txt 
+done < $lista 
 rm -f $destino
 #Uncomment next line to generate C.S graphs using GRACE in batch mode [script graphs.gr is attached]
 #xmgrace -batch graphs.gr -nosafe -hardcopy -log xy
